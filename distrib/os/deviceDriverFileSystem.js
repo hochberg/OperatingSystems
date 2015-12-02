923f0612c43183e20904e5a8f9b5500d5a3207c2
hochberg <alexander.hochberg1@marist.edu>
2015-12-01 19:24:23 -0500

    FS - Fix Read Overflow Starting to Fix

--- distrib/os/deviceDriverFileSystem.js
+++ distrib/os/deviceDriverFileSystem.js
@@ -173,78 +173,67 @@ var TSOS;
             _StdOut.putText("Successfully Created: " + filename);
         };
         DeviceDriverFileSystem.prototype.writeToFile = function (filename, data) {
-            console.log(filename);
-            console.log(data);
             //loop through all of directory to find filename
-            /////////
             //finds next available directory
             var foundFileMeta;
             dance: for (var y = 0; y < this.sector; y++) {
                 for (var z = 0; z < this.block; z++) {
-                    //if block is not in use it becomes next avil directory
-                    console.log(this.removeTrail(sessionStorage.getItem("b0" + y + z).slice(4, (sessionStorage.getItem("b0" + y + z).length))));
-                    console.log(this.stringToHex(filename));
+                    //if block is not in use it becomes next avail directory
                     if (this.removeTrail(sessionStorage.getItem("b0" + y + z).slice(4, (sessionStorage.getItem("b0" + y + z).length)))
                         == this.stringToHex(filename)) {
+                        //set found meta data of filename
                         foundFileMeta = sessionStorage.getItem("b0" + y + z).slice(1, 4);
-                        console.log(foundFileMeta);
                         //breaks loop
                         break dance;
                     }
                 }
             }
-            //checks if data will have overflow
-            console.log(data.length);
-            //   if(data.length > this.bytes){
+            //initalizes data array (in case of overflow)
             var dataArray = [];
+            //converts data to hex
             var hexData = this.stringToHex(data);
+            //splits data an array of parts (60 char each = bytes - meta)
             for (var x = 0; x < hexData.length; x) {
                 var firstDataSeg = hexData.slice(x, (x + (this.bytes - 4)));
                 dataArray.push(firstDataSeg);
-                console.log(hexData);
-                console.log(dataArray);
                 x = x + (this.bytes - 4);
             }
-            // }
-            //if data doesn't have overflow
-            //  else{
-            console.log(foundFileMeta);
-            //without quotation marks
-            //sessionStorage.setItem("b" + foundFileMeta, this.addTrail("1" + foundFileMeta + this.stringToHex(data.slice(1, data.length - 1))));
-            console.log(this.addTrail("1" + foundFileMeta + dataArray[0]).length);
+            //if no overflow, meta data doesn't change
             var meta = "---";
-            //if overflow
+            //if overflow, sets to next avail file
             if (!(dataArray[1] == null)) {
                 meta = this.getNextAvailableFile();
             }
-            //TODO
-            //dont add found meta data, but instead the next available dir
+            //sets data file in storage
+            //of first file (in case of overflow)
             sessionStorage.setItem("b" + foundFileMeta, this.addTrail("1" + meta + dataArray[0]));
-            console.log(dataArray);
+            //takes first elements off of array
             dataArray.shift();
-            console.log(dataArray);
-            console.log(this.hexToString(sessionStorage.getItem("b" + foundFileMeta)));
-            console.log(this.hexToString(sessionStorage.getItem("b" + foundFileMeta)).length);
+            //if ther are more elements in data array ( overflow )
             if (!(dataArray[0] == null)) {
+                //loops through data array
                 for (var x = 0; x < dataArray.length; x) {
+                    //updates mbr to next avil file
                     this.updateMbr();
+                    //initalizes next meta if no more overflow
                     var nextMeta = "---";
-                    console.log(dataArray);
+                    //if there is more overflow, finds next avail file for meta
                     if (!(dataArray[1] == null)) {
                         nextMeta = this.getNextAvailableFile();
                     }
+                    //sets next chunk of data in storage
                     sessionStorage.setItem("b" + meta, this.addTrail("1" + nextMeta + dataArray[0]));
+                    //pulls first ele from data array
                     dataArray.shift();
+                    //meta becomes next meta for rest of loop
                     meta = nextMeta;
                 }
             }
-            // }
             //prints hard drive
             _Display.printFullHardDrive();
-            //
-            //TODO make sure this works
             //updates mbr
             this.updateMbr();
+            //prints success message to console
             _StdOut.putText("Successfully Written To: " + filename);
         };
         DeviceDriverFileSystem.prototype.inFileNameArray = function (filename) {
@@ -273,12 +262,20 @@ var TSOS;
                 }
             }
             console.log(foundFileMeta);
-            var dataWithMeta = this.removeTrail(sessionStorage.getItem("b" + foundFileMeta));
-            console.log(dataWithMeta);
-            var data = dataWithMeta.slice(4, dataWithMeta.length);
-            console.log(data);
-            var dataToString = this.hexToString(data);
-            _StdOut.putText(filename + " reads: " + dataToString);
+            var rawData = sessionStorage.getItem("b" + foundFileMeta);
+            console.log(rawData.slice(1, 4));
+            if (rawData.slice(1, 4) === "---") {
+                console.log("word");
+                var dataWithMeta = this.removeTrail(rawData);
+                console.log(dataWithMeta);
+                var data = dataWithMeta.slice(1, dataWithMeta.length);
+                console.log(data);
+                var dataToString = this.hexToString(data);
+                _StdOut.putText(filename + " reads: " + dataToString);
+            }
+            else {
+                console.log("overflow");
+            }
         };
         return DeviceDriverFileSystem;
     })(TSOS.DeviceDriver);
